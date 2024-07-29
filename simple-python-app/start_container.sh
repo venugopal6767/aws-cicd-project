87fc045077bae44dc99765a422e7259679806f89
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-07-29 13:26:18 +0530

    deploy

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull venuzs/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 venuzs/simple-python-flask-app:latest
