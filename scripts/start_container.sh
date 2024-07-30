57483561ed2a2e8bd11bd321d510d00870b5aa59
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-07-30 08:32:12 +0530

    a

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,12 +5,13 @@ echo "Starting the Docker container..."
 
 # Define your container name and image URI
 CONTAINER_NAME="awscicd"
-IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
+# IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
 
 # Pull the latest image
-docker pull $IMAGE_URI
+# docker pull $IMAGE_URI
+docker pull venuzs/simple-python-app:0a9d47cef3
 
 # Run the new container
-docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE_URI
+docker run -d --name $CONTAINER_NAME -p 5000:5000 venuzs/simple-python-app:0a9d47cef3
 
 echo "Container $CONTAINER_NAME started with image $IMAGE_URI."
