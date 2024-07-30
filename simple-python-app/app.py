07da4b64cf3031a83a87eedd06640b06a4d79bb6
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-07-30 17:08:09 +0530

    neew

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, venugopal!'
+    return 'Hello, venugopal ga!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5500)
