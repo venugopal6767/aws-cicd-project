9c8d750f87590977adc2701f9cec10668de0db8d
venu67776 <83317608+venu67776@users.noreply.github.com>
2024-07-30 09:32:44 +0530

    venu

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,14 +3,17 @@
 # Start the Docker container
 echo "Starting the Docker container..."
 
-# Define your container name and image URI
-# IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
+# Define the container name and get the image URI from imagedefinitions.json
+CONTAINER_NAME="my-pythonapp"  # Set this to your container name
+IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
 
 # Pull the latest image
-# docker pull $IMAGE_URI
-docker pull nginx
+echo "Pulling image $IMAGE_URI..."
+docker pull $IMAGE_URI
 
 # Run the new container
-docker run -d --name nginx -p 80:80 nginx:latest
+echo "Running container $CONTAINER_NAME with image $IMAGE_URI..."
+docker run -d --name $CONTAINER_NAME -p 5500:5500 $IMAGE_URI
 
-echo "Container $CONTAINER_NAME started with image"
+echo "Container $CONTAINER_NAME started with image $IMAGE_URI"
+
--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, world from aws cicd venugopal !'
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5500)
+
 
